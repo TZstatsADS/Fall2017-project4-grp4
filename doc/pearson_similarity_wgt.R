47a28d0fabe1641451b39415c0e355cf0d300db1
Xinyao <31713753+haidy081028@users.noreply.github.com>
2017-11-22 23:12:18 -0500

    pearson weight matrix

---
+++ doc/pearson_similarity_wgt.R
@@ -0,0 +1,5 @@
+setwd("D:/Fall 2017/ADS/eachmovie_sample")
+train2<-read.csv("train2.csv",header=T)
+train2<-data.frame(train2[,-1],row.names = train2[,1])
+mv_cor<- cor(t(train2[,-1]),use="pairwise.complete.obs",method="pearson")
+write.csv(mv_cor,"pearson2.csv")
