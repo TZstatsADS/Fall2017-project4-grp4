b7f0aa5093d3c688a615362de7fa581cec706166
Xinyao <31713753+haidy081028@users.noreply.github.com>
2017-11-23 00:25:26 -0500

    Pearson Similarity for both datasets (code)

--- doc/pearson_similarity_wgt.R
+++ doc/pearson_similarity_wgt.R
@@ -1,4 +1,11 @@
-setwd("D:/Fall 2017/ADS/eachmovie_sample")
+###Dataset 1
+train<-read.csv("MS_train2.csv",header=T)
+train<-data.frame(train[,-c(1,2)],row.names = train[,2])
+train[is.na(train)]=0
+mv_cor<- cor(t(train),use="pairwise.complete.obs",method="pearson")
+write.csv(mv_cor,"pearson1.csv")
+
+### Dataset 2
 train2<-read.csv("train2.csv",header=T)
 train2<-data.frame(train2[,-1],row.names = train2[,1])
 mv_cor<- cor(t(train2[,-1]),use="pairwise.complete.obs",method="pearson")
