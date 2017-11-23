e58fdaf01802882a5473e64cf00c5074966c3969
Xinyao <31713753+haidy081028@users.noreply.github.com>
2017-11-23 02:12:31 -0500

    mse similarity for both datasets (code)

--- doc/mse_similarity_wgt.R
+++ doc/mse_similarity_wgt.R
@@ -1,6 +1,26 @@
+###Dataset 1
+train<-read.csv("MS_train2.csv",header=T)
+train<-data.frame(train[,-c(1,2)],row.names = train[,2])
+train[is.na(train)]=0
+###MSE=(a-b)^2=a^2+b^2-2ab
+i_matrix<-!is.na(train)
+#2ab
+dff<-train
+dff[is.na(dff)]=0
+two.a.b<-2*as.matrix(dff) %*% t(dff)
+sqr<-train^2
+sqr[is.na(sqr)]=0
+a.square<-sqr %*% t(i_matrix)
+b.square<-i_matrix %*% t(sqr)
+se<-a.square+b.square-two.a.b
+common.no<-i_matrix %*% t(i_matrix)
+mse<-se/common.no
+mean.square.diff<-as.data.frame(mse)
+write.csv(mean.square.diff,"mse.csv")
+
+###Dataset 2
 train2<-read.csv("train2.csv",header=T)
 train2<-data.frame(train2[,-1],row.names = train2[,1])
-###MSE=(a-b)^2=a^2+b^2-2ab
 i_matrix<-!is.na(train2)
 #2ab
 dff<-train2
@@ -15,3 +35,5 @@ common.no<-i_matrix %*% t(i_matrix)
 mse<-se/common.no
 mean.square.diff<-as.data.frame(mse)
 write.csv(mean.square.diff,"mse2.csv")
+
+
