350a87e4e02140e2ba54d7d37128e810d8721473
Lin Han <lh2810@columbia.edu>
2017-11-26 19:23:16 -0500

    Update combined.R

--- lib/combined.R
+++ lib/combined.R
@@ -1,10 +1,10 @@
 combined<- function(sw,n,t){
   loc<- apply(sw, 1, function(m) order(abs(m), decreasing = TRUE) )
   loc1<- loc[1:n, ]
-  result<- list()
-  for(i in 1:5055){
-    result[[i]]<- ifelse((sw[i, loc1[ ,i]])&(abs(sw[i, loc1[ ,i]])>=t), sw[i, loc1[ ,i]], NA)
-    result[[i]]<- na.omit(result[[i]])
+  result<- matrix(NA,nrow = nrow(sw),ncol = ncol(sw))
+  result<- data.frame(result)
+  for(i in 1:nrow(result)){
+    result[ i, loc1[ ,i]]<- ifelse(abs(sw[i, loc1[ ,i]])>=t,sw[i, loc1[ ,i]],NA)
   }
   return(result)
 }
