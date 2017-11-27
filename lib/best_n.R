42366ebcd1347dc23fba3f39f78ef2f97de62445
Lin Han <lh2810@columbia.edu>
2017-11-26 19:22:52 -0500

    Update best_n.R

--- lib/best_n.R
+++ lib/best_n.R
@@ -1,9 +1,10 @@
 best_n<- function(sw,n){
   loc<- apply(sw, 1, function(m) order(abs(m), decreasing = TRUE) )
   loc1<- loc[1:n, ]
-  result<- list()
-  for(i in 1:5055){
-    result[[i]]<- ifelse(sw[i, loc1[ ,i]],sw[i, loc1[ ,i]],NA )
+  result<- matrix(NA,nrow = nrow(sw),ncol = ncol(sw))
+  result<- data.frame(result)
+  for(i in 1:nrow(result)){
+    result[ i, loc1[ ,i] ]<- sw[i, loc1[ ,i]]
   }
   return(result)
 }
