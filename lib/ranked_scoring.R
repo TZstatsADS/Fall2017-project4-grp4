3062ec39b542e614857727a0f09980399b0d0328
Lin Han <lh2810@columbia.edu>
2017-11-29 00:49:10 -0500

    Update ranked_scoring.R

--- lib/ranked_scoring.R
+++ lib/ranked_scoring.R
@@ -1,15 +1,17 @@
-ranked_s<- function(pred,d,n){
-  pred<- ifelse(is.na(pred), d, pred)
-  index<- apply(pred[ ,-1], 1, function(m) sort(abs(m), decreasing = TRUE))
-  index<- data.frame(index, row.names = pred[ ,1])
-  alpha<- apply(pred[ ,-1], 1, function(x) sum(x==0.5))
-  XX<- function(x){
-    for(j in 1:length(x)){
-      x*2^(1-j)*2^(alpha[j]-1)
-    }
-  }
-  m<- max(pred-0,d)
-  r<- apply(m, 1,XX)
-  R<- 100* sum(r)* sum(sort(r,decreasing = T)[1:n])
+ranked_s<- function(pred,test,d=0,alpha=10){
+  test<- apply(test, 2, function(x) ifelse(is.na(x),d,x))
+  
+  loc<- apply(pred[ ,-1], 1,function(x) names(sort(x, decreasing = T)))
+  loc<-t(loc)
+  loc<- data.frame(loc, row.names = pred[ ,1])
+  
+  loc1<- apply(test[ ,-1], 1,function(x) names(sort(x, decreasing = T)))
+  loc1<-t(loc1)
+  loc1<- data.frame(loc1, row.names = test[ ,1])
+  
+  m<-apply(test,2, function(x) ifelse(x-d>0, x-d,0))
+  r<- m*2^(alpha-1)*2^(1-loc)
+  # r<- apply(m, 1,XX)
+  # R<- 100* sum(r)* sum(sort(r,decreasing = T)[1:n])
   return(R)
 }
